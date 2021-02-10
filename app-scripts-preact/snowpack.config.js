c4f9c32a3101f2926013bd667195a5621d1a2107
wuxianqiang <2631640352@qq.com>
2021-02-10 15:52:13 +0800

    first commit

---
+++ app-scripts-preact/snowpack.config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const cwd = process.cwd();
+const isTS = fs.existsSync(path.join(cwd, 'tsconfig.json'));
+
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: ['@snowpack/plugin-babel', '@prefresh/snowpack', '@snowpack/plugin-dotenv'],
+  packageOptions: {},
+};
---
+++ app-scripts-react/snowpack.config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const cwd = process.cwd();
+const isTS = fs.existsSync(path.join(cwd, 'tsconfig.json'));
+
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-babel',
+    '@snowpack/plugin-dotenv',
+    ...(isTS ? ['@snowpack/plugin-typescript'] : []),
+  ],
+  devOptions: {},
+  packageOptions: {},
+};
